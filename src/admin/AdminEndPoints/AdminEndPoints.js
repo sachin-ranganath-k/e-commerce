452873c042d217ca00892a5cbfc4559282505ef7
ranganathnk <ranganathnkulkarni@gmail.com>
2023-01-04 12:22:13 +0530

    updated endpoint

--- src/admin/AdminEndPoints/AdminEndPoints.js
+++ src/admin/AdminEndPoints/AdminEndPoints.js
@@ -1,10 +1,10 @@
- export const FETCH_ADMIN_API="https://gangadharpoly.org/ecom/admin/admin_login/get_admin_login.php"
+ export const FETCH_ADMIN_API="http://gangadharpoly.org/ecom/admin/admin_login/get_admin_login.php"
 
 
-export const TODAY_ORDERS_API="https://gangadharpoly.org/ecom/admin/order_list/get_today_order_list.php"
+export const TODAY_ORDERS_API="http://gangadharpoly.org/ecom/admin/order_list/get_today_order_list.php"
  
- export const SHOW_CATEGORY_API="https://gangadharpoly.org/ecom/admin/category/get_category.php"
- export const ADD_CATEGORY_API="https://gangadharpoly.org/ecom/admin/category/insert_category_api.php"
+ export const SHOW_CATEGORY_API="http://gangadharpoly.org/ecom/admin/category/get_category.php"
+ export const ADD_CATEGORY_API="http://gangadharpoly.org/ecom/admin/category/insert_category_api.php"
 
 
 
