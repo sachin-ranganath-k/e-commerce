a5f063c53bc581775bdcf5de749296949d9c01b8
Sachin (Ranganath) K <ranganathkulkarni20@gmail.com>
2023-04-02 12:30:00 +0530

    Update AdminEndPoints.js

--- src/admin/AdminEndPoints/AdminEndPoints.js
+++ src/admin/AdminEndPoints/AdminEndPoints.js
@@ -28,37 +28,37 @@ export const FETCH_ADMIN_API ="https://localhost/ecom/admin/admin_login/get_admi
 //Orders
 export const TODAY_ORDERS_API ="https://localhost/ecom/admin/order_list/get_today_order_list.php";
 export const TOTAL_ORDERS_API ="https://localhost/ecom/admin/order_list/get_total_order_list.php";
-export const UPDATE_ORDER_DELIVERED_API = "https://localhost/ecom/admin/order_list/update_order_delivered.php";
+export const UPDATE_ORDER_DELIVERED_API = "https://gangadharpoly.org/ecom/admin/order_list/update_order_delivered.php";
 
 
 //Categories
-export const SHOW_CATEGORY_API ="https://localhost/ecom/admin/category/get_category.php";
-export const ADD_CATEGORY_API ="https://localhost/ecom/admin/category/insert_category_api.php";
+export const SHOW_CATEGORY_API ="https://gangadharpoly.org/ecom/admin/category/get_category.php";
+export const ADD_CATEGORY_API ="https://gangadharpoly.org/ecom/admin/category/insert_category_api.php";
 
 //Brands
-export const SHOW_BRAND_API ="https://localhost/ecom/admin/brand/get_brand.php";
-export const ADD_BRAND_API ="https://localhost/ecom/admin/brand/insert_brand_api.php";
+export const SHOW_BRAND_API ="https://gangadharpoly.org/ecom/admin/brand/get_brand.php";
+export const ADD_BRAND_API ="https://gangadharpoly.org/ecom/admin/brand/insert_brand_api.php";
 
 
 //Products
-export const ADD_PRODUCT_API ="https://localhost/ecom/admin/product_react/insert_product_api.php";
-export const FETCH_PRODUCTS_API ="https://localhost/ecom/admin/product%20_api/get_products_api.php";
+export const ADD_PRODUCT_API ="https://gangadharpoly.org/ecom/admin/product_react/insert_product_api.php";
+export const FETCH_PRODUCTS_API ="https://gangadharpoly.org/ecom/admin/product%20_api/get_products_api.php";
 
 
 //Cart
-export const ADD_TO_CART_API="https://localhost/ecom/user/cart/add_cart_api.php";
-export const FETCH_ITEMS_FROM_CART_OF_PERSON_API="https://localhost/ecom/user/cart/get_cart_api.php";
-export const DELETE_ITEM_FROM_CART_API="https://localhost/ecom/user/cart/delete_cart_api.php";
-export const UPDATE_ITEM_QUANTITY_API="https://localhost/ecom/user/cart/put_cart_api.php";
-export const CLEAR_CART_API="https://localhost/ecom/user/cart/clear_cart_api.php"
+export const ADD_TO_CART_API="https://gangadharpoly.org/ecom/user/cart/add_cart_api.php";
+export const FETCH_ITEMS_FROM_CART_OF_PERSON_API="https://gangadharpoly.org/ecom/user/cart/get_cart_api.php";
+export const DELETE_ITEM_FROM_CART_API="https://gangadharpoly.org/ecom/user/cart/delete_cart_api.php";
+export const UPDATE_ITEM_QUANTITY_API="https://gangadharpoly.org/ecom/user/cart/put_cart_api.php";
+export const CLEAR_CART_API="https://gangadharpoly.org/ecom/user/cart/clear_cart_api.php"
 
 
 
 
 //User
-export const REGISTER_USER_SUCCESS_API="http://localhost/ecom/user/user/user_register_api.php";
-export const GET_REGISTERED_USERS_API="http://localhost/ecom/user/user/get_user_register_api.php";
+export const REGISTER_USER_SUCCESS_API="http://gangadharpolo.org/ecom/user/user/user_register_api.php";
+export const GET_REGISTERED_USERS_API="http://gangadharpolo.org/ecom/user/user/get_user_register_api.php";
 
 //Orders
-export const ORDER_NOW_API="http://localhost/ecom/user/order/add_order.php";
-export const ORDERS_LIST_API="http://localhost/ecom/user/order/get_orders_list_api.php";
+export const ORDER_NOW_API="http://gangadharpolo.org/ecom/user/order/add_order.php";
+export const ORDERS_LIST_API="http://gangadharpolo.org/ecom/user/order/get_orders_list_api.php";
